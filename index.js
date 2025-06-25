3a2d54f9d575bf0d1cd9ab6a74ab7703b9355038
adirathod1822 <80950864+adirathod1822@users.noreply.github.com>
2025-06-25 09:27:09 +0530

    onlychat.uk in cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://onlychat-1f5ee.web.app"],
+    origin: ["https://onlychat-1f5ee.web.app", "onlychat.uk"],
     methods: ["GET", "POST"],
   },
 });
